f09b232e0702b3a1409af038536c7b4b08feb2f8
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-02 07:29:21 +0100

    fix

---
+++ day1/target/debug/deps/day1-5a43f7f56bcb8f40.d
@@ -0,0 +1,6 @@
+c:\Users\woute\OneDrive\Documents\Code\Advent2022\advent2022\day1\target\debug\deps\day1-5a43f7f56bcb8f40.rmeta: src\main.rs src\../input.txt
+
+c:\Users\woute\OneDrive\Documents\Code\Advent2022\advent2022\day1\target\debug\deps\day1-5a43f7f56bcb8f40.d: src\main.rs src\../input.txt
+
+src\main.rs:
+src\../input.txt:
