01010ad13070c6bf696a7b9de26d862468e7f50f
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-02 09:16:50 +0100

    2b

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,18 +1,19 @@
 fn main() {
-    let score = include_str!("../input.txt")
+    let (sum_a, sum_b) = include_str!("../input.txt")
         .lines()
         .map(|l| l.split_once(" ").unwrap())
-        .fold(0, |acc, (x, y)| match (x, y) {
-            ("A", "X") => acc + 4,
-            ("A", "Y") => acc + 8,
-            ("A", "Z") => acc + 3,
-            ("B", "X") => acc + 1,
-            ("B", "Y") => acc + 5,
-            ("B", "Z") => acc + 9,
-            ("C", "X") => acc + 7,
-            ("C", "Y") => acc + 2,
-            ("C", "Z") => acc + 6,
+        .fold((0, 0), |(acc_a, acc_b), (he, me)| match (he, me) {
+            ("A", "X") => (acc_a + 3 + 1, acc_b + 0 + 3),
+            ("A", "Y") => (acc_a + 6 + 2, acc_b + 3 + 1),
+            ("A", "Z") => (acc_a + 0 + 3, acc_b + 6 + 2),
+            ("B", "X") => (acc_a + 0 + 1, acc_b + 0 + 1),
+            ("B", "Y") => (acc_a + 3 + 2, acc_b + 3 + 2),
+            ("B", "Z") => (acc_a + 6 + 3, acc_b + 6 + 3),
+            ("C", "X") => (acc_a + 6 + 1, acc_b + 0 + 2),
+            ("C", "Y") => (acc_a + 0 + 2, acc_b + 3 + 3),
+            ("C", "Z") => (acc_a + 3 + 3, acc_b + 6 + 1),
             _ => unreachable!(),
         });
-    println!("a1: {}", score)
+    println!("2a: {}", sum_a);
+    println!("2b: {}", sum_b)
 }
