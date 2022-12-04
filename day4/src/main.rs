9def97ac258dfa77291ca1c6c09aa262ec9693ab
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-04 23:08:29 +0100

    comments

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
     let lines = include_str!("../input.txt");
 
-    let overlap_a = parse_input(lines)
+    let full_overlap_count = parse_input(lines)
         .into_iter()
+        // to be fully encompassed by another range, both the high and low of
+        // range1 need to be within the high and low of range2
+        // thus: a1 <= b1 <= a2 && a1 <= b2 <= a2
+        //       but also the other way around:
+        //       b1 <= a1 <= b2 && b1 <= a2 <= b2
+        // resulting in (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2
         .filter(|assignments| {
             (assignments[0] >= assignments[2] && assignments[1] <= assignments[3])
                 || (assignments[0] <= assignments[2] && assignments[1] >= assignments[3])
         })
         .count();
-    println!("4a: {}", overlap_a);
+    println!("4a: {}", full_overlap_count);
 
-    let overlap_b = parse_input(lines)
+    let partial_overlap_count = parse_input(lines)
         .into_iter()
         // char needs to be in both ranges
         // assume: a1 <= char <= a2 and
@@ -20,7 +26,7 @@ fn main() {
         // then: a1 <= b2 and b1 <= a2
         .filter(|assignments| assignments[0] <= assignments[3] && assignments[2] <= assignments[1])
         .count();
-    println!("4b: {}", overlap_b);
+    println!("4b: {}", partial_overlap_count);
 }
 
 fn parse_input(input: &str) -> Vec<Vec<i32>> {
