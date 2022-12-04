fdabc310b043a797060b88399f187f7ee338e09d
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-04 08:05:44 +0100

    3a

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let sum_priorities = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.split_at(l.len() / 2))
+        .map(|(a, b)| {
+            let mut duplicate_char = '\0';
+            for char in a.chars() {
+                if b.contains(char) {
+                    duplicate_char = char;
+                }
+            }
+            return duplicate_char;
+        })
+        .map(|char| {
+            if char.is_lowercase() {
+                return char as i32 - 96;
+            } else {
+                return char as i32 - 38;
+            }
+        })
+        .sum::<i32>();
+
+    println!("2a: {}", sum_priorities);
 }
