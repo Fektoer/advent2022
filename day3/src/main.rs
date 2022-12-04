21d9b96103c8fd3fed53e8286c936c9362380a4e
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-04 08:30:33 +0100

    3b

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,14 +11,43 @@ fn main() {
             }
             return duplicate_char;
         })
-        .map(|char| {
-            if char.is_lowercase() {
-                return char as i32 - 96;
-            } else {
-                return char as i32 - 38;
-            }
-        })
+        .map(|char| calc_priority(char))
         .sum::<i32>();
 
     println!("2a: {}", sum_priorities);
+
+    let mut three_group = Vec::new();
+    let mut groups = Vec::new();
+    let rucksacks = include_str!("../input.txt").lines();
+    for rucksack in rucksacks {
+        three_group.push(rucksack);
+        if three_group.len() == 3 {
+            groups.push(three_group);
+            three_group = Vec::new();
+        }
+    }
+
+    let sum_priorities_group = groups
+        .iter()
+        .map(|group| {
+            let mut duplicate_char = '\0';
+            for char in group[0].chars() {
+                if group[1].contains(char) && group[2].contains(char) {
+                    duplicate_char = char;
+                }
+            }
+            return duplicate_char;
+        })
+        .map(|char| calc_priority(char))
+        .sum::<i32>();
+
+    println!("2a: {}", sum_priorities_group);
+}
+
+fn calc_priority(value: char) -> i32 {
+    if value.is_lowercase() {
+        return value as i32 - 96;
+    } else {
+        return value as i32 - 38;
+    }
 }
