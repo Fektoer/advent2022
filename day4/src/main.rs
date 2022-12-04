06bfafdb7a418019f7dfd2d3831f128ff095b688
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-04 23:14:13 +0100

    better comment

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,12 +3,11 @@ fn main() {
 
     let full_overlap_count = parse_input(lines)
         .into_iter()
-        // to be fully encompassed by another range, both the high and low of
-        // range1 need to be within the high and low of range2
+        // range1 needs to be fully encompassed by another range, or vice versa
         // thus: a1 <= b1 <= a2 && a1 <= b2 <= a2
         //       but also the other way around:
         //       b1 <= a1 <= b2 && b1 <= a2 <= b2
-        // resulting in (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2
+        // resulting in: (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2
         .filter(|assignments| {
             (assignments[0] >= assignments[2] && assignments[1] <= assignments[3])
                 || (assignments[0] <= assignments[2] && assignments[1] >= assignments[3])
@@ -19,11 +18,11 @@ fn main() {
     let partial_overlap_count = parse_input(lines)
         .into_iter()
         // char needs to be in both ranges
-        // assume: a1 <= char <= a2 and
-        //         b1 <= char <= b2
+        // thus: a1 <= char <= a2 and
+        //       b1 <= char <= b2
         // assume each range is well formed:
-        // a1 <= a2 and b1 <= b2
-        // then: a1 <= b2 and b1 <= a2
+        //       a1 <= a2 and b1 <= b2
+        // resulting in: a1 <= b2 and b1 <= a2
         .filter(|assignments| assignments[0] <= assignments[3] && assignments[2] <= assignments[1])
         .count();
     println!("4b: {}", partial_overlap_count);
