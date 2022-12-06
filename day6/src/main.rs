7d64f7c5b1681c3fc47433561ebc783a60e25dd5
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-06 11:26:23 +0100

    beter

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 fn main() {
     let input = include_str!("../input.txt");
     println!("6a: {}", analyze_window(input, 4));
@@ -8,21 +9,17 @@ fn analyze_window(input: &str, size: i32) -> i32 {
     let input_as_vec = input.chars().collect::<Vec<char>>();
     let mut windows = input_as_vec.windows(size as usize).enumerate();
 
-    loop {
-        let (index, value) = windows.next().unwrap();
-        if value.len() == 0 {
-            return index as i32;
-        } else {
-            if contains_only_unique(value.to_vec()) {
-                return index as i32 + (size);
-            }
-        }
-    }
+    if let Some((index, _)) = windows.find(|window| {
+        let (_, value) = window;
+        contains_only_unique(value.to_vec())
+    }) {
+        return index as i32 + size;
+    } else {
+        return 0;
+    };
 }
 
 fn contains_only_unique(vec_of_char: Vec<char>) -> bool {
-    let mut cloned_vec = vec_of_char.clone();
-    cloned_vec.sort();
-    cloned_vec.dedup();
-    vec_of_char.len() == cloned_vec.len()
+    let cloned_set: HashSet<char> = HashSet::from_iter(vec_of_char.iter().cloned());
+    vec_of_char.len() == cloned_set.len()
 }
