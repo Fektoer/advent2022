f25a6d7e389851104aae0721760e0a9174b44239
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-08 00:32:31 +0100

    day7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,59 +1,63 @@
 use regex::Regex;
-use std::collections::HashMap;
 
 fn main() {
-    let mut directories: HashMap<&str, i32> = HashMap::new();
-    let mut commands = include_str!("../temp.txt").lines().collect::<Vec<&str>>();
+    let mut directories: Vec<(&str, i32)> = Vec::new();
+    let mut commands = include_str!("../input.txt").lines().collect::<Vec<&str>>();
 
-    recursive_search(&mut commands, &mut directories, &mut "", &mut 0);
+    let _line = commands.drain(0..1).next().unwrap();
+    recursive_search(&mut commands, &mut directories, "/");
 
-    for (key, value) in directories {
-        println!("{}: {}", key, value);
-    }
+    let sum: i32 = directories
+        .iter()
+        .filter(|dir| dir.1 <= 100000)
+        .map(|dir| dir.1)
+        .sum();
+    println!("7a: {}", sum);
+
+    directories.sort_by(|a, b| a.1.cmp(&b.1));
+    let total_free = 70000000 - directories.last().unwrap().1;
+    let total_needed = 30000000 - total_free;
+
+    let min_size = directories
+        .into_iter()
+        .find(|dir| dir.1 > total_needed)
+        .unwrap()
+        .1;
+    println!("7b: {}", min_size);
 }
 
 fn recursive_search<'a>(
     commands: &mut Vec<&'a str>,
-    directories: &mut HashMap<&'a str, i32>,
+    directories: &mut Vec<(&'a str, i32)>,
     dir_name: &'a str,
-    file_size: &mut i32,
-) {
-    if commands.len() == 0 {
-        return;
-    }
-    let line = commands.drain(0..1).next().unwrap();
+) -> i32 {
+    let mut dir_size = 0;
 
-    let re_dir = Regex::new(r"^dir ([a-z]+)").unwrap();
-    match re_dir.captures(line) {
-        Some(_) => recursive_search(commands, directories, dir_name, file_size),
-        None => {}
-    }
+    while commands.len() != 0 {
+        let line = commands.drain(0..1).next().unwrap();
 
-    let re_file = Regex::new(r"(\d+)\s").unwrap();
-    match re_file.captures(line) {
-        Some(caps) => {
-            let size = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
-            *file_size = *file_size + size;
-            recursive_search(commands, directories, dir_name, file_size)
+        let re_file = Regex::new(r"(\d+)\s").unwrap();
+        match re_file.captures(line) {
+            Some(caps) => {
+                dir_size += caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            }
+            None => {}
         }
-        None => {}
-    }
 
-    let re_cd = Regex::new(r"\$ cd ([a-z]+|/)").unwrap();
-    match re_cd.captures(line) {
-        Some(caps) => {
-            let name = caps.get(1).unwrap().as_str();
-            recursive_search(commands, directories, name, file_size)
+        let re_cd = Regex::new(r"\$ cd ([a-z]+|/)").unwrap();
+        match re_cd.captures(line) {
+            Some(caps) => {
+                let new_dir = caps.get(1).unwrap().as_str();
+                dir_size += recursive_search(commands, directories, new_dir)
+            }
+            None => {}
         }
-        None => {}
-    }
 
-    if line == "$ cd .." {
-        directories.insert(dir_name, *file_size);
-        recursive_search(commands, directories, dir_name, &mut 0)
+        if line == "$ cd .." {
+            directories.push((dir_name, dir_size));
+            return dir_size;
+        }
     }
-
-    if line == "$ ls" {
-        recursive_search(commands, directories, dir_name, file_size)
-    }
+    directories.push((dir_name, dir_size));
+    return dir_size;
 }
