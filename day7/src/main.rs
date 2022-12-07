6d77fc2264c78da51d83433c8dd1505a97b9bd14
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-08 00:58:27 +0100

    comments

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let _line = commands.drain(0..1).next().unwrap();
     recursive_search(&mut commands, &mut directories, "/");
 
+    // 7a: Filter all directories with size <= 10000 and sum those
     let sum: i32 = directories
         .iter()
         .filter(|dir| dir.1 <= 100000)
@@ -14,6 +15,7 @@ fn main() {
         .sum();
     println!("7a: {}", sum);
 
+    // 7b: Find the first folder we can delete to have enough space for the update
     directories.sort_by(|a, b| a.1.cmp(&b.1));
     let total_free = 70000000 - directories.last().unwrap().1;
     let total_needed = 30000000 - total_free;
@@ -31,11 +33,14 @@ fn recursive_search<'a>(
     directories: &mut Vec<(&'a str, i32)>,
     dir_name: &'a str,
 ) -> i32 {
+    // Initialize the directory size to 0
     let mut dir_size = 0;
 
     while commands.len() != 0 {
+        // Drain the next command
         let line = commands.drain(0..1).next().unwrap();
 
+        // If it's a file, add the file size to the directory total
         let re_file = Regex::new(r"(\d+)\s").unwrap();
         match re_file.captures(line) {
             Some(caps) => {
@@ -44,6 +49,7 @@ fn recursive_search<'a>(
             None => {}
         }
 
+        // If it's a change directory command, change the directory and start recursion
         let re_cd = Regex::new(r"\$ cd ([a-z]+|/)").unwrap();
         match re_cd.captures(line) {
             Some(caps) => {
@@ -53,11 +59,15 @@ fn recursive_search<'a>(
             None => {}
         }
 
+        // If we move out one level, push the directory plus it's size to storage and exit the recursion,
+        // returning the size of this directory and all underlying ones
         if line == "$ cd .." {
             directories.push((dir_name, dir_size));
             return dir_size;
         }
     }
+
+    // No more commands, push what we have to storage and exit recursion
     directories.push((dir_name, dir_size));
     return dir_size;
 }
