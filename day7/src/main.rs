0b323d661f54d2d2a37605446a5644b049687e6d
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-08 01:15:12 +0100

    naming

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,9 +3,7 @@ use regex::Regex;
 fn main() {
     let mut directories: Vec<(&str, i32)> = Vec::new();
     let mut commands = include_str!("../input.txt").lines().collect::<Vec<&str>>();
-
-    let _line = commands.drain(0..1).next().unwrap();
-    recursive_search(&mut commands, &mut directories, "/");
+    search_folder(&mut commands, &mut directories, "");
 
     // 7a: Filter all directories with size <= 10000 and sum those
     let sum: i32 = directories
@@ -28,10 +26,10 @@ fn main() {
     println!("7b: {}", min_size);
 }
 
-fn recursive_search<'a>(
+fn search_folder<'a>(
     commands: &mut Vec<&'a str>,
     directories: &mut Vec<(&'a str, i32)>,
-    dir_name: &'a str,
+    root: &'a str,
 ) -> i32 {
     // Initialize the directory size to 0
     let mut dir_size = 0;
@@ -53,8 +51,8 @@ fn recursive_search<'a>(
         let re_cd = Regex::new(r"\$ cd ([a-z]+|/)").unwrap();
         match re_cd.captures(line) {
             Some(caps) => {
-                let new_dir = caps.get(1).unwrap().as_str();
-                dir_size += recursive_search(commands, directories, new_dir)
+                let new_root = caps.get(1).unwrap().as_str();
+                dir_size += search_folder(commands, directories, new_root)
             }
             None => {}
         }
@@ -62,7 +60,7 @@ fn recursive_search<'a>(
         // If we move out one level, push the directory plus it's size to storage and exit the recursion,
         // returning the size of this directory and all underlying ones
         if line == "$ cd .." {
-            directories.push((dir_name, dir_size));
+            directories.push((root, dir_size));
             return dir_size;
         }
 
@@ -70,6 +68,6 @@ fn recursive_search<'a>(
     }
 
     // No more commands, push what we have to storage and exit recursion
-    directories.push((dir_name, dir_size));
+    directories.push((root, dir_size));
     return dir_size;
 }
