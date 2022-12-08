c48c54ca7711b877cc3184a087eaafa082b3cb0b
Wouter Luijten <wouterluijten@hotmail.com>
2022-12-08 11:15:27 +0100

    naming

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let tree_grid = include_str!("../input.txt")
+    let forest = include_str!("../input.txt")
         .lines()
         .map(|line| {
             line.chars()
@@ -8,48 +8,48 @@ fn main() {
         })
         .collect::<Vec<Vec<i32>>>();
 
-    println!("8a {}", calc_visible_trees(tree_grid.clone()));
-    println!("8b {}", calc_max_scenic(tree_grid.clone()));
+    println!("8a {}", calc_visible_trees(forest.clone()));
+    println!("8b {}", calc_max_scenic(forest.clone()));
 }
 
-fn calc_max_scenic(tree_grid: Vec<Vec<i32>>) -> i32 {
+fn calc_max_scenic(forest: Vec<Vec<i32>>) -> i32 {
     let mut scenic_vec: Vec<i32> = Vec::new();
 
-    for row in tree_grid.iter().enumerate() {
-        for tree in row.1.iter().enumerate() {
+    for row_of_trees in forest.iter().enumerate() {
+        for tree in row_of_trees.1.iter().enumerate() {
             let mut horizontal_index = tree.0.clone();
             let mut left_visible = true;
             let mut left_count = 0;
             while horizontal_index != 0 && left_visible {
                 horizontal_index -= 1;
-                left_visible = row.1[horizontal_index] < *tree.1;
+                left_visible = row_of_trees.1[horizontal_index] < *tree.1;
                 left_count += 1;
             }
 
             horizontal_index = tree.0.clone();
             let mut right_visible = true;
             let mut right_count = 0;
-            while horizontal_index < row.1.len() - 1 && right_visible {
+            while horizontal_index < row_of_trees.1.len() - 1 && right_visible {
                 horizontal_index += 1;
-                right_visible = row.1[horizontal_index] < *tree.1;
+                right_visible = row_of_trees.1[horizontal_index] < *tree.1;
                 right_count += 1;
             }
 
-            let mut vertical_index = row.0.clone();
+            let mut vertical_index = row_of_trees.0.clone();
             let mut top_visible = true;
             let mut top_count = 0;
             while vertical_index != 0 && top_visible {
                 vertical_index -= 1;
-                top_visible = tree_grid[vertical_index][tree.0] < *tree.1;
+                top_visible = forest[vertical_index][tree.0] < *tree.1;
                 top_count += 1;
             }
 
-            vertical_index = row.0.clone();
+            vertical_index = row_of_trees.0.clone();
             let mut bot_visible = true;
             let mut bot_count = 0;
-            while vertical_index < tree_grid.len() - 1 && bot_visible {
+            while vertical_index < forest.len() - 1 && bot_visible {
                 vertical_index += 1;
-                bot_visible = tree_grid[vertical_index][tree.0] < *tree.1;
+                bot_visible = forest[vertical_index][tree.0] < *tree.1;
                 bot_count += 1;
             }
 
@@ -59,15 +59,15 @@ fn calc_max_scenic(tree_grid: Vec<Vec<i32>>) -> i32 {
     return *scenic_vec.iter().max().unwrap();
 }
 
-fn calc_visible_trees(tree_grid: Vec<Vec<i32>>) -> i32 {
+fn calc_visible_trees(forest: Vec<Vec<i32>>) -> i32 {
     let mut visible = 0;
-    for row in tree_grid.iter().enumerate() {
-        for tree in row.1.iter().enumerate() {
+    for row_of_trees in forest.iter().enumerate() {
+        for tree in row_of_trees.1.iter().enumerate() {
             // edges
             if tree.0 == 0
-                || tree.0 == row.1.len() - 1
-                || row.0 == 0
-                || row.0 == tree_grid.len() - 1
+                || tree.0 == row_of_trees.1.len() - 1
+                || row_of_trees.0 == 0
+                || row_of_trees.0 == forest.len() - 1
             {
                 visible += 1;
             } else {
@@ -75,29 +75,29 @@ fn calc_visible_trees(tree_grid: Vec<Vec<i32>>) -> i32 {
                 let mut left_visible = true;
                 while horizontal_index != 0 && left_visible {
                     horizontal_index -= 1;
-                    left_visible = row.1[horizontal_index] < *tree.1;
+                    left_visible = row_of_trees.1[horizontal_index] < *tree.1;
                 }
 
                 horizontal_index = tree.0.clone();
                 let mut right_visible = true;
-                while horizontal_index < row.1.len() - 1 && right_visible {
+                while horizontal_index < row_of_trees.1.len() - 1 && right_visible {
                     horizontal_index += 1;
-                    right_visible = row.1[horizontal_index] < *tree.1;
+                    right_visible = row_of_trees.1[horizontal_index] < *tree.1;
                 }
 
-                let mut vertical_index = row.0.clone();
+                let mut vertical_index = row_of_trees.0.clone();
                 let mut top_visible = true;
                 while vertical_index != 0 && top_visible {
                     vertical_index -= 1;
 
-                    top_visible = tree_grid[vertical_index][tree.0] < *tree.1
+                    top_visible = forest[vertical_index][tree.0] < *tree.1
                 }
 
-                vertical_index = row.0.clone();
+                vertical_index = row_of_trees.0.clone();
                 let mut bot_visible = true;
-                while vertical_index < tree_grid.len() - 1 && bot_visible {
+                while vertical_index < forest.len() - 1 && bot_visible {
                     vertical_index += 1;
-                    bot_visible = tree_grid[vertical_index][tree.0] < *tree.1
+                    bot_visible = forest[vertical_index][tree.0] < *tree.1
                 }
 
                 if left_visible || right_visible || top_visible || bot_visible {
